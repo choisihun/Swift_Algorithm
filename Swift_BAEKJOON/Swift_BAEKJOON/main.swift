b8b3798fd9e8b66b559efcc27a24572587cba0f6
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-01-17 23:30:25 +0900

    .

--- Swift_BAEKJOON/Swift_BAEKJOON/main.swift
+++ Swift_BAEKJOON/Swift_BAEKJOON/main.swift
@@ -11,10 +11,26 @@ import Foundation
 //print("Hello, World!")
 
 //1000번 문제
-let line = readLine()!
-let lineArr = line.components(separatedBy: " ")
-let a = Int(lineArr[0])!
-let b = Int(lineArr[1])!
-print(a+b)
+//let line = readLine()!
+//let lineArr = line.components(separatedBy: " ")
+//let a = Int(lineArr[0])!
+//let b = Int(lineArr[1])!
+//print(a+b)
 
+//1001번 문제
+//let line = readLine()!
+//let lineArr = line.components(separatedBy: " ")
+//let a = Int(lineArr[0])!
+//let b = Int(lineArr[1])!
+//print(a-b)
 
+//10998번 문제
+//let line = readLine()!
+//let lineArr = line.components(separatedBy: " ")
+//let a = Int(lineArr[0])!
+//let b = Int(lineArr[1])!
+//print(a*b)
+
+//1008번 문제
+//let arr = readLine()!.components(separatedBy: " ").map{ Double($0)! }
+//print(arr[0] / arr[1])
